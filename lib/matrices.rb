a7fc8f2c179ac8d4c2e5aeb241b54a715a2c4293
HERNANDEZ ROUSSEAU <alu4084@cc407.(none)>
2013-11-06 10:27:47 +0000

    multiplicacion

--- lib/matrices.rb
+++ lib/matrices.rb
@@ -1,5 +1,5 @@
 class Matriz
-        attr_accessor :N, :M, :contenido #Get & Set
+        attr_accessor :N, :M, :cont #Get & Set
         
         def initialize(n,m)           
                 @N, @M = n, m
@@ -19,4 +19,42 @@ class Matriz
                           
                 @cont[i] = other                        
         end
+
+     def to_s
+          s = ""
+      i = 0
+      while(i < @N)
+        j = 0
+        while(j < @M)
+              s += "#{cont[i][j]}\t"
+              j += 1
+            end
+            s += "\n"
+            i += 1
+      end
+          s
+        end
+
+     def *(other)
+ 
+      c = Matriz.new(@N, other.M)
+      i = 0
+      while(i < @N)
+        j = 0
+        while(j < other.M)
+              k = 0
+              c[i][j] = 0
+              while(k < @M)
+                c[i][j] =  c[i][j] + cont[i][k] * other[k][j]
+                k += 1
+              end 
+          j += 1
+        end 
+            i += 1
+      end 
+     
+   
+    c
+  end
 end
+
--- spec/matrices_spec.rb
+++ spec/matrices_spec.rb
@@ -4,6 +4,9 @@ describe Matriz do
                 @m1 = Matriz.new(2,2)
                 @m1[0] = [2,3]
                 @m1[1] = [3,5]
+		@m3 = Matriz.new(2,3)
+                @m3[0] = [1,2,3]
+                @m3[1] = [4,5,6]
           end
         describe " # Almacenamiento" do
                 it " #Numero de filas " do
@@ -19,4 +22,12 @@ describe Matriz do
                         @m1[0][1].should eq(3)
                 end
          end
+
+   describe " # Multiplicacion. " do
+        it " # Se deben poder multiplicar matrices" do
+        (@m1 * @m3).to_s.should == "14\t19\t24\t\n23\t31\t39\t\n"
+   
+      end
+end
+
 end
